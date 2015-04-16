c790919c6b37452dd99b0441b323fb30351da342
Ward Fisher <ward.fisher@gmail.com>
2015-04-16 23:46:47 +0200

    Added new docker project

---
+++ dockerfile-nctests/README.md
@@ -0,0 +1,6 @@
+When run, this will run tests for:
+
+* netcdf-c
+* netcdf-fortran
+* netcdf-cxx4
+
---
+++ dockerfile-nctests/run_all_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+###
+# Check out all the projects.
+###
+git clone http://www.github.com/Unidata/netcdf-c
+git clone http://www.github.com/Unidata/netcdf-fortran
+git clone http://www.github.com/Unidata/netcdf-cxx4
+
+###
+# Build & test netcdf-c, then install it so it
+# can be used by the other projects.
+###
+
+cd /root
+
+mkdir build-netcdf-c && cd build-netcdf-c && cmake /root/netcdf-c -DCMAKE_INSTALL_PREFIX=/usr && make Experimental
+make install
+
+###
+# Build & test netcdf-fortran
+###
+
+cd /root
+mkdir build-netcdf-fortran && cd build-netcdf-fortran && cmake /root/netcdf-fortran && make Experimental
+
+###
+# Build & test netcdf-cxx4.
+###
