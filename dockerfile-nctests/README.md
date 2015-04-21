2d979774bf432c6a76d119c657cc8fa6b96ee638
Ward Fisher <wfisher@unidata.ucar.edu>
2015-04-21 14:07:30 -0600

    Parameterized tests so that we could specify an alternate branch for netcdf-c.

--- dockerfile-nctests/README.md
+++ dockerfile-nctests/README.md
@@ -6,4 +6,8 @@ When this docker container is run, it will check out the following packages from
 
 Each package will be built and tested.  This way, we can see if any changes in `netcdf-c` break anything which depends on it (`netcdf-fortran` and `netcdf-cxx4`).
 
+## Specifying an Alternative Branch
 
+You can specify an alternative branch for `netcdf-c` than `master` using the following syntax.
+
+    $ docker run -e CBRANCH="branch name" wardf/nctests
--- dockerfile-nctests/run_all_tests.sh
+++ dockerfile-nctests/run_all_tests.sh
@@ -8,13 +8,24 @@ git clone http://www.github.com/Unidata/netcdf-fortran
 git clone http://www.github.com/Unidata/netcdf-cxx4
 
 ###
+# Check out the branch
+# specified by "CBRANCH"
+###
+
+cd netcdf-c
+git fetch
+git checkout $CBRANCH
+
+###
 # Build & test netcdf-c, then install it so it
 # can be used by the other projects.
 ###
 
 cd /root
 
-mkdir build-netcdf-c && cd build-netcdf-c && cmake /root/netcdf-c -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_HDF4=ON -DENABLE_EXTRA_TESTS=ON && make Experimental && make install
+mkdir build-netcdf-c && cd build-netcdf-c && cmake /root/netcdf-c -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_HDF4=ON -DENABLE_EXTRA_TESTS=ON && make Experimental
+
+make install
 
 
 ###
