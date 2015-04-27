ec2815cea8f09c10c282d91801da3562fab964bb
Ward Fisher <wfisher@unidata.ucar.edu>
2015-04-27 14:40:22 -0600

    More reorganization. Updated, renamed utility scripts.

--- dockerfile-nctests/build_all_images.sh
+++ dockerfile-nctests/build_all_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t wardf/nctests -f Dockerfile .
-docker build -t wardf/nctests:64 -f Dockerfile .
-docker build -t wardf/nctests:32 -f Dockerfile.32bit .
+docker build -t wardf/nctests:base -f Dockerfile.base .
+docker build -t wardf/nctests -f Dockerfile.serial .
+docker build -t wardf/nctests:serial -f Dockerfile.serial .
 docker build -t wardf/nctests:openmpi -f Dockerfile.openmpi .
 docker build -t wardf/nctests:mpich -f Dockerfile.mpich .
--- dockerfile-nctests/push_all_images.sh
+++ dockerfile-nctests/push_all_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+docker push wardf/nctests:base
 docker push wardf/nctests
-docker push wardf/nctests:64
-docker push wardf/nctests:32
+docker push wardf/nctests:serial
 docker push wardf/nctests:openmpi
 docker push wardf/nctests:mpich
