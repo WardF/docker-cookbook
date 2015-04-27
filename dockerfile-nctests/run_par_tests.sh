8ed83b874a886d9ab13429ebe3f4edaefc11ee60
Ward Fisher <wfisher@unidata.ucar.edu>
2015-04-27 10:30:54 -0600

    Added skeleton files for a parallel test image.

---
+++ dockerfile-nctests/run_par_tests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+###
+# Check out all the projects.
+###
+git clone http://www.github.com/Unidata/netcdf-c
+git clone http://www.github.com/Unidata/netcdf-fortran
+git clone http://www.github.com/Unidata/netcdf-cxx4
+
+###
+# Check out the branch
+# specified by "CBRANCH", "FBRANCH", "CXXBRANCH"
+###
+
+cd netcdf-c
+git fetch
+git checkout $CBRANCH
+cd /root
+
+cd netcdf-fortran
+git fetch
+git checkout $FBRANCH
+cd /root
+
+cd netcdf-cxx4
+git fetch
+git checkout $CXXBRANCH
+cd /root
+
+
+###
+# Build & test netcdf-c, then install it so it
+# can be used by the other projects.
+###
+
+cd /root
+
+mkdir build-netcdf-c && cd build-netcdf-c && cmake /root/netcdf-c -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_HDF4=ON -DENABLE_EXTRA_TESTS=ON -DENABLE_MMAP=ON -DBUILDNAME_PREFIX="docker" -DBUILDNAME_SUFFIX="$CBRANCH" && make Experimental
+
+make install
+
+
+###
+# Build & test netcdf-fortran
+###
+
+cd /root
+mkdir build-netcdf-fortran && cd build-netcdf-fortran && cmake /root/netcdf-fortran -DBUILDNAME_PREFIX="docker" -DBUILDNAME_SUFFIX="$FBRANCH" && make Experimental
+
+
+###
+# Build & test netcdf-cxx4.
+###
+
+cd /root
+mkdir build-netcdf-cxx4 && cd build-netcdf-cxx4 && cmake /root/netcdf-cxx4 -DBUILD_NAME_PREFIX="docker" -DBUILD_NAME_SUFFIX="$CXXBRANCH" && make Experimental
