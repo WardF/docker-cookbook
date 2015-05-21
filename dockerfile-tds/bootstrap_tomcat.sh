fc8a17bea3a31323ee45a2deeca4eb142226ddfa
Ward Fisher <ward.fisher@gmail.com>
2015-05-21 14:22:05 -0600

    Cleaned up a bit based on input from thredds group.

--- dockerfile-tds/bootstrap_tomcat.sh
+++ dockerfile-tds/bootstrap_tomcat.sh
@@ -6,13 +6,11 @@
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 set -x
-apache2ctl start
-sleep 3
 /etc/init.d/tomcat start
 
 sleep 10
+set +x
 
-# For now, just sleep really long to avoid exiting. Fix later.
 echo ""
 
 echo "[Hit ENTER key to exit] or run 'docker stop <container>'"
