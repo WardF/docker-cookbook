e5feefc450805d654b71a6164a31e7ec1ad5061f
Ward Fisher <wfisher@unidata.ucar.edu>
2015-06-03 10:43:14 -0600

    Parameterized build script to take alternative image name prefixes.

--- dockerfile-nctests/build_all_images.sh
+++ dockerfile-nctests/build_all_images.sh
@@ -3,27 +3,36 @@
 dohelp ()
 {
     echo ""
-    echo "Usage: $0 [-i|-x]"
+    echo "Usage: $0 -[ix] -p [image name prefix]"
     echo -e "\t -i     Build 32-bit images."
     echo -e "\t -x     Build 64-bit images."
+    echo -e "\t -p     Specify an image name. Default: wardf/nctests"
     echo ""
 }
 
 DO32=""
 DO64=""
+IPREF="wardf/nctests"
 
 if [ $# -lt 1 ]; then
     dohelp
     exit 0
 fi
 
-while getopts "ix" o; do
+while getopts "ixp:" o; do
     case "${o}" in
         i)
             DO32="TRUE"
             ;;
         x)
             DO64="TRUE"
+            ;;
+        p)
+            IPREF=${OPTARG}
+            if [ "x$IPREF" == "x" ]; then
+                dohelp
+                exit 0
+            fi
             ;;
         *)
             dohelp
@@ -34,21 +43,21 @@ done
 if [ "x$DO32" == "xTRUE" ]; then
     echo "Building 32-bit images."
 
-    docker build -t wardf/nctests:base32 -f Dockerfile.base32 .
-    docker build -t wardf/nctests:serial32 -f Dockerfile.serial32 .
-    docker build -t wardf/nctests:openmpi32 -f Dockerfile.openmpi32 .
-    docker build -t wardf/nctests:mpich32 -f Dockerfile.mpich32 .
+    docker build -t $IPREF:base32 -f Dockerfile.base32 .
+    docker build -t $IPREF:serial32 -f Dockerfile.serial32 .
+    docker build -t $IPREF:openmpi32 -f Dockerfile.openmpi32 .
+    docker build -t $IPREF:mpich32 -f Dockerfile.mpich32 .
 
 fi
 
 if [ "x$DO64" == "xTRUE" ]; then
     echo "Building 64-bit images."
 
-    docker build -t wardf/nctests:base -f Dockerfile.base .
-    docker build -t wardf/nctests -f Dockerfile.serial .
-    docker build -t wardf/nctests:serial -f Dockerfile.serial .
-    docker build -t wardf/nctests:openmpi -f Dockerfile.openmpi .
-    docker build -t wardf/nctests:mpich -f Dockerfile.mpich .
+    docker build -t $IPREF:base -f Dockerfile.base .
+    docker build -t $IPREF -f Dockerfile.serial .
+    docker build -t $IPREF:serial -f Dockerfile.serial .
+    docker build -t $IPREF:openmpi -f Dockerfile.openmpi .
+    docker build -t $IPREF:mpich -f Dockerfile.mpich .
 
 fi
 
