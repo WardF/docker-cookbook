be149734194a9fe1e21a3fc58ab040d72a1c097a
Ward Fisher <wfisher@ucar.edu>
2015-06-04 16:21:31 -0600

    Added a final window that shows the stats for the running processes.

--- dockerfile-nctests/run_all_images.sh
+++ dockerfile-nctests/run_all_images.sh
@@ -81,3 +81,5 @@ if [ "x$DO64" == "xTRUE" ]; then
 
     runtest mpich wardf/nctests:mpich $BRANCH
 fi
+
+xterm -T "Docker Stats" -bg black -fg white -geometry 140x20+10+10 -e docker stats $(docker ps -q) &
