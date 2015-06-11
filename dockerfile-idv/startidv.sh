67024dfb958d98be48ab7c55dc1060675d2f1013
Ward Fisher <wfisher@unidata.ucar.edu>
2015-06-11 15:33:53 -0600

    Tweaked configuration.

--- dockerfile-idv/startidv.sh
+++ dockerfile-idv/startidv.sh
@@ -26,3 +26,10 @@ xinit -- /usr/bin/Xvfb :1 -screen 0 $SIZEH\x$SIZEW\x$CDEPTH &
 sleep 5
 export DISPLAY=localhost:1
 /home/idv/IDV/runIDV
+
+echo ""
+echo "[Hit ENTER key to exit] or run 'docker stop <container>'"
+
+read
+
+echo "Stopping."
