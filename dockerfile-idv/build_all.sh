7d79162ed7d17c47bcfdcfa43f86e638f667d08e
Ward Fisher <wfisher@unidata.ucar.edu>
2015-07-27 11:24:31 -0600

    Corrected typo.

--- dockerfile-idv/build_all.sh
+++ dockerfile-idv/build_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-build -t unidata/idv-base -f Dockerfile.base .
-build -t unidata/idv-gui -f Dockerfile.gui .
+docker build -t unidata/idv-base -f Dockerfile.base .
+docker build -t unidata/idv-gui -f Dockerfile.gui .
