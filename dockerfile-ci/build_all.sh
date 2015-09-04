ae60f584db93e8a33f24ebfb83a94c18d74e83d1
Ward Fisher <ward.fisher@gmail.com>
2015-09-04 14:09:18 -0600

    Added utopic build, some utility scripts.

---
+++ dockerfile-ci/build_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xterm -bg black -fg white -T "[Ubuntu Vivid: 64-bit]" -e "docker build -t unidata/ncci:vivid-x64 -f Dockerfile.vivid.x64 ."
+sleep 1
+
+xterm -bg black -fg white -T "[Ubuntu Utopic: 64-bit]" -e "docker build -t unidata/ncci:utopic-x64 -f Dockerfile.utopic.x64 ."
+sleep 1
