5ad139ef06dfe274a073830ebec0e35303ecd7b6
Ward Fisher <ward.fisher@gmail.com>
2015-09-04 13:55:52 -0600

    Additional changes.

--- dockerfile-ci/cmake-scripts/ctest_service.serial.sh
+++ dockerfile-ci/cmake-scripts/ctest_service.serial.sh
@@ -5,10 +5,12 @@ set -e
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 ctest -V -S CI.cmake > ccontinuous_test.out 2>&1 &
-ctest -V -S FCI.cmake > fcontinuous_test.out 2>&1 &
-ctest -V -S CXX4I.cmake > cxx4continuous_test.out 2>&1 &
+#ctest -V -S FCI.cmake > fcontinuous_test.out 2>&1 &
+#ctest -V -S CXX4I.cmake > cxx4continuous_test.out 2>&1 &
 
 echo "[Continuous Integration Tests in Progress. Press Enter 3x to Exit]"
 read
+echo "[Continuous Integration Tests in Progress. Press Enter 2x to Exit]"
 read
+echo "[Continuous Integration Tests in Progress. Press Enter to Exit]"
 read
