a586e08e55a573270123e56c3254f4d8f827604a
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-08 14:23:09 -0600

    More deploying from template.

--- dockerfile-ci/refresh_dockerfiles.sh
+++ dockerfile-ci/refresh_dockerfiles.sh
@@ -3,11 +3,11 @@
 # Utility script to recreate dockerfiles from a template.
 #
 
-./create_dockerfile ubuntu:vivid vivid.x64
-./create_dockerfile f69m/ubuntu32:vivid vivid.x86
+./create_dockerfile.sh ubuntu:vivid vivid.x64
+./create_dockerfile.sh "f69m\/ubuntu32:vivid" vivid.x86
 
-./create_dockerfile ubuntu:utopic utopic.x64
-./create_dockerfile f69m/ubuntu32:14.10 utopic.x86
+./create_dockerfile.sh ubuntu:utopic utopic.x64
+./create_dockerfile.sh "f69m\/ubuntu32:14.10" utopic.x86
 
-./create_dockerfile ubuntu:trusty trusty.x64
-./create_dockerfile f69m/ubuntu32:trusty trusty.x86
+./create_dockerfile.sh ubuntu:trusty trusty.x64
+./create_dockerfile.sh "f69m\/ubuntu32:trusty" trusty.x86
