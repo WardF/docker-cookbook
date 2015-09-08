af8bf83700a930076f906f0e3869d49a11bc762a
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-08 10:16:10 -0600

    Added a couple 32-bit images, also added centos7 image.

--- dockerfile-ci/build_all.sh
+++ dockerfile-ci/build_all.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 xterm -bg black -fg white -T "[Ubuntu Vivid: 64-bit]" -e "docker build -t unidata/ncci:vivid-x64 -f Dockerfile.vivid.x64 ." &
-sleep 1
+sleep 3
+
+xterm -bg black -fg white -T "[Ubuntu Vivid: 32-bit]" -e "docker build -t unidata/ncci:vivid-x86 -f Dockerfile.vivid.x86 ." &
+sleep 3
 
 xterm -bg black -fg white -T "[Ubuntu Utopic: 64-bit]" -e "docker build -t unidata/ncci:utopic-x64 -f Dockerfile.utopic.x64 ." &
-sleep 1
+sleep 3
 
-xterm -bg black -fg white -T "[Ubuntu trusty: 64-bit]" -e "docker build -t unidata/ncci:trusty-x64 -f Dockerfile.utopic.x64 ." &
-sleep 1
+xterm -bg black -fg white -T "[Ubuntu Trusty: 64-bit]" -e "docker build -t unidata/ncci:trusty-x64 -f Dockerfile.utopic.x64 ." &
+sleep 3
+
+xterm -bg black -fg white -T "[Fedora 21: 64-bit]" -e "docker build -t unidata/ncci:fedora21-x64 -f Dockerfile.fedora21.x64 . " &
+sleep 3
+
+xterm -bg black -fg white -T "[Centos 7: 64-bit]" -e "docker build -t unidata/ncci:centos7-x64 -f Dockerfile.centos7.x64 . " &
+sleep 3
+
+
+echo "[Finished]"
--- dockerfile-ci/run_all.sh
+++ dockerfile-ci/run_all.sh
@@ -3,8 +3,17 @@
 xterm -bg black -fg white -T "[Ubuntu Vivid: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h vivid-x64-docker unidata/ncci:vivid-x64"&
 sleep 3
 
+xterm -bg black -fg white -T "[Ubuntu Vivid: 21-bit]" -geometry 100x10 -e "docker run --rm -it -h vivid-x86-docker unidata/ncci:vivid-x86"&
+sleep 3
+
 xterm -bg black -fg white -T "[Ubuntu Utopic: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h utopic-x64-docker unidata/ncci:utopic-x64"&
 sleep 3
 
 xterm -bg black -fg white -T "[Ubuntu Trusty: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h trusty-x64-docker unidata/ncci:trusty-x64"&
+sleep 3
+
+xterm -bg black -fg white -T "[Fedora 21: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h trusty-x64-docker unidata/ncci:fedora21-x64"&
+sleep 3
+
+xterm -bg black -fg white -T "[Centos 7: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h trusty-x64-docker unidata/ncci:centos7-x64"&
 sleep 3
