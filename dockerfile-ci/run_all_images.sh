6280ce0bff4a30e23c3a7a71996f0b9854b02d71
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-08 15:44:30 -0600

    Corrected a typo. 32-bit, not 21-bit.

--- dockerfile-ci/run_all_images.sh
+++ dockerfile-ci/run_all_images.sh
@@ -18,7 +18,7 @@ fi
 xterm -bg black -fg white -T "[Ubuntu Vivid: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h vivid-x64-$RSHORT unidata/ncci:vivid-x64"&
 sleep 3
 
-xterm -bg black -fg white -T "[Ubuntu Vivid: 21-bit]" -geometry 100x10 -e "docker run --rm -it -h vivid-x86-$RSHORT unidata/ncci:vivid-x86"&
+xterm -bg black -fg white -T "[Ubuntu Vivid: 32-bit]" -geometry 100x10 -e "docker run --rm -it -h vivid-x86-$RSHORT unidata/ncci:vivid-x86"&
 sleep 3
 
 xterm -bg black -fg white -T "[Ubuntu Utopic: 64-bit]" -geometry 100x10 -e "docker run --rm -it -h utopic-x64-$RSHORT unidata/ncci:utopic-x64"&
