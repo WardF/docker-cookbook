f3697f0f72833f51d9c1d78b8758e738b8e6aa7b
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-09 11:06:41 -0600

    Removed sleep in build script, added cron invocation to CMD.

--- dockerfile-ci/build_all_images.sh
+++ dockerfile-ci/build_all_images.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Vivid: 64-bit]" -e "docker build -t unidata/ncci:vivid-x64 -f Dockerfile.vivid.x64 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Vivid: 32-bit]" -e "docker build -t unidata/ncci:vivid-x86 -f Dockerfile.vivid.x86 . ; echo "" &&  echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Utopic: 64-bit]" -e "docker build -t unidata/ncci:utopic-x64 -f Dockerfile.utopic.x64 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Utopic: 32-bit]" -e "docker build -t unidata/ncci:utopic-x86 -f Dockerfile.utopic.x86 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Trusty: 64-bit]" -e "docker build -t unidata/ncci:trusty-x64 -f Dockerfile.trusty.x64 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Ubuntu Trusty: 32-bit]" -e "docker build -t unidata/ncci:trusty-x86 -f Dockerfile.trusty.x86 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Fedora 21: 64-bit]" -e "docker build -t unidata/ncci:fedora21-x64 -f Dockerfile.fedora21.x64 . ; echo "" &&  echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 xterm -geometry 100x10 -bg black -fg white -T "[Centos 7: 64-bit]" -e "docker build -t unidata/ncci:centos7-x64 -f Dockerfile.centos7.x64 . ; echo "" && echo '[Press Return to Close]' && read " &
-sleep 3
+
 
 
 echo "[Finished]"
