cf61176e9ebd2a6626f829eda15720957314f335
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-09 12:49:36 -0600

    Modified some tags, added fedora:22

--- dockerfile-ci/recreate_dockerfiles.sh
+++ dockerfile-ci/recreate_dockerfiles.sh
@@ -6,17 +6,18 @@
 ###
 # Create Ubuntu Dockerfiles.
 ###
-./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "ubuntu:vivid" vivid.x64
-./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:vivid" vivid.x86
+./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "ubuntu:vivid" vivid-x64
+./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:vivid" vivid-x86
 
 ./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "ubuntu:utopic" utopic.x64
-./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:14.10" utopic.x86
+./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:14.10" utopic-x86
 
 ./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "ubuntu:trusty" trusty.x64
-./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:trusty" trusty.x86
+./create_dockerfile.sh templates/Dockerfile.ubuntu.generic "f69m\/ubuntu32:trusty" trusty-x86
 
 ###
 # Create Fedora and Centos Dockerfiles
 ###
-./create_dockerfile.sh templates/Dockerfile.fedora.generic "fedora:21" fedora21.x64
-./create_dockerfile.sh templates/Dockerfile.fedora.generic "centos:7" centos7.x64
+./create_dockerfile.sh templates/Dockerfile.fedora.generic "fedora:21" fedora21-x64
+./create_dockerfile.sh templates/Dockerfile.fedora.generic "fedora:22" fedora22-x64
+./create_dockerfile.sh templates/Dockerfile.fedora.generic "centos:7" centos7-x64
