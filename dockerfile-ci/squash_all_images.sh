4dee538632469a413ef4cb4aa3fcb3ae90e3272e
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-10 11:11:45 -0600

    Adding stuff to let us save/squash images.

---
+++ dockerfile-ci/squash_all_images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Depends on docker-squash utility found at:
+# - https://github.com/jwilder/docker-squash
+#
+# Note that there is a markdown file with OSX-specific
+# instructions.
+
+set -e
+
+PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+
+IMGS="unidata/ncci:vivid-x64 \
+unidata/ncci:vivid-x86 \
+unidata/ncci:utopic-x64 \
+unidata/ncci:utopic-x86 \
+unidata/ncci:trusty-x64 \
+unidata/ncci:trusty-x86 \
+unidata/ncci:fedora22-x64 \
+unidata/ncci:fedora21-x64 \
+unidata/ncci:centos7-x64"
+
+
+for X in $IMGS; do
+    echo $X
+    set -x
+    docker save $X | sudo TMPDIR=/sandbox/tmp docker-squash -verbose -t $X-squashed | docker load
+    set +x
+done
