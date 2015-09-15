a607afbe12a230c514dc09935085926041dea687
Ward Fisher <ward.fisher@gmail.com>
2015-09-15 11:23:18 -0600

    Added a way to pass optional commands to meld.

--- dockerfile-meld/run_meld.sh
+++ dockerfile-meld/run_meld.sh
@@ -8,7 +8,7 @@ if [ $# -lt 4 ]; then
     docker images
     echo ""
     echo "You must specify an OS and a docker image."
-    echo "example: $0 linux wardf/meld [directory 1] [directory 2]"
+    echo "example: $0 linux wardf/meld [directory 1] [directory 2] [Optional Command]"
     echo "OS Options:"
     echo -e "\tlinux"
     echo -e "\tosx"
@@ -33,9 +33,9 @@ set -x
 
 if [ "X$1" == "Xosx" ]; then
     MYIP=$(curl ipecho.net/plain)
-    docker run --rm -e DISPLAY=$MYIP:0 --net=host -v $3:/former -v $4:/latter $2
+    docker run --rm -e DISPLAY=$MYIP:0 --net=host -v $3:/former -v $4:/latter $2 $5
 fi
 
 if [ "X$1" == "Xlinux" ]; then
-    docker run --rm -e DISPLAY --net=host -v $3:/former -v $4:/latter -v $HOME/.Xauthority:/root/.Xauthority $2
+    docker run --rm -e DISPLAY --net=host -v $3:/former -v $4:/latter -v $HOME/.Xauthority:/root/.Xauthority $2 $5
 fi
