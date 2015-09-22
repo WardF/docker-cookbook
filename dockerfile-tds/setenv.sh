6d2494e26ce0aef5084624265cb7b1526241ddb8
Ward Fisher <wfisher@unidata.ucar.edu>
2015-09-22 11:28:24 -0600

    Switched tomcat version to using environmental variables to make changing it easier.

--- dockerfile-tds/setenv.sh
+++ dockerfile-tds/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/usr/lib/jvm/default-java
-export JAVA_OPTS="-Xmx4096m -Xms512m -server -Djava.awt.headless=true -Djava.util.prefs.systemRoot=$CATALINA_BASE/content/thredds/javaUtilPrefs -Dtds.content.root.path=/usr/local/apache-tomcat-8.0.22/content"
-export CATALINE_BASE="/usr/local/apache-tomcat-8.0.22/"
+export JAVA_OPTS="-Xmx4096m -Xms512m -server -Djava.awt.headless=true -Djava.util.prefs.systemRoot=$CATALINA_BASE/content/thredds/javaUtilPrefs -Dtds.content.root.path=/usr/local/${TC_NAME}/content"
+export CATALINE_BASE="/usr/local/${TC_NAME}"
