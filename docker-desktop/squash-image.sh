6f8f05560b67df3072bcb21a33618588d12bcbfa
Ward Fisher <ward.fisher@gmail.com>
2015-09-30 15:35:27 -0600

    Tweaked squash script.

--- docker-desktop/squash-image.sh
+++ docker-desktop/squash-image.sh
@@ -5,6 +5,13 @@
 
 set -e
 
+PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+
+echo "======"
+echo "Using tar: $(which tar)"
+echo "======"
+echo ""
+
 docker save wardf/desktop | sudo docker-squash -verbose -t wardf/desktop -o wardf_desktop.tar
 sudo chown wfisher:wfisher wardf_desktop.tar
 
