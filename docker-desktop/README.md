c2859748dba7f9feae9111e72680323296c68e4d
Ward Fisher <ward.fisher@gmail.com>
2015-09-30 14:18:20 -0600

    Added a basic 'desktop environment' docker recipe.

---
+++ docker-desktop/README.md
@@ -0,0 +1,4 @@
+# Docker-Desktop
+
+A docker image which creates a VNC/noVNC desktop environment, as a replacement for a virtual machine.
+
---
+++ docker-desktop/startdesktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Script that starts the docker image desktop
+# session. Not for internal use.
+#
+
+set -e
+
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+xinit -- /usr/bin/Xvfb :1 -screen 0 $SIZEH\x$SIZEW\x$CDEPTH &
+sleep 5
+
+export DISPLAY=localhost:1
+
+if [ "x${USENOVNC}" == "xTRUE" ]; then
+    mv /home/${CUSER}/self.pem /home/${CUSER}/noVNC/utils/
+    /home/${CUSER}/noVNC/utils/launch.sh --vnc 127.0.0.1:5901 &
+fi
+
+echo "Press [return] to exit."
+read
