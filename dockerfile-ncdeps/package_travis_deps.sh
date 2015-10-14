b9db195447f62f09d7ae2a5f17feb637d933d7f0
Ward Fisher <wfisher@ucar.edu>
2015-10-13 20:52:47 -0600

    Modified to reflect latest version of cmake.

--- dockerfile-ncdeps/package_travis_deps.sh
+++ dockerfile-ncdeps/package_travis_deps.sh
@@ -24,7 +24,7 @@ set -e
 # Define the files we need to package.
 ##
 
-CMAKEFILES="/usr/doc/cmake-3.2 /usr/share/cmake-3.2 /usr/bin/cmake /usr/bin/ctest /usr/bin/cpack"
+CMAKEFILES="/usr/doc/cmake-3.3 /usr/share/cmake-3.3 /usr/bin/cmake /usr/bin/ctest /usr/bin/cpack"
 
 HDFFILES="/usr/lib/libhdf4.settings /usr/lib/libdf* /usr/lib/libmfhdf* /usr/include/hdf2netcdf.h /usr/include/local_nc.h /usr/include/mfhdf.h /usr/include/hdf4_netcdf.h /usr/include/mfhdfi.h /usr/include/mfdatainfo.h /usr/include/atom.h /usr/include/bitvect.h /usr/include/cdeflate.h /usr/include/cnbit.h /usr/include/cnone.h /usr/include/cskphuff.h /usr/include/crle.h /usr/include/cszip.h /usr/include/df.h /usr/include/dfan.h /usr/include/dfi.h /usr/include/dfgr.h /usr/include/dfrig.h /usr/include/dfsd.h /usr/include/dfstubs.h /usr/include/dfufp2i.h /usr/include/dynarray.h /usr/include/H4api_adpt.h /usr/include/h4config.h /usr/include/hbitio.h /usr/include/hchunks.h /usr/include/hcomp.h /usr/include/hcompi.h /usr/include/hconv.h /usr/include/hdf.h /usr/include/hdfi.h /usr/include/herr.h /usr/include/hfile.h /usr/include/hkit.h /usr/include/hlimits.h /usr/include/hproto.h /usr/include/hntdefs.h /usr/include/htags.h /usr/include/linklist.h /usr/include/mfan.h /usr/include/mfgr.h /usr/include/mstdio.h /usr/include/tbbt.h /usr/include/vattr.h /usr/include/vg.h /usr/include/hdatainfo.h"
 
