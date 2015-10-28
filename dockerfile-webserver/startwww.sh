b177db24c7a475345bc15c9a99bf20c1ce08fe13
Ward Fisher <wfisher@ucar.edu>
2015-10-27 22:05:10 -0600

    Fleshed out webserver.

---
+++ dockerfile-webserver/startwww.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#
+
+set -e
+
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+if [ "x$HELP" != "x" ]; then
+    cat README.md
+    exit
+fi
+
+if [ ! -d /www ]; then
+    echo "Missing /www directory."
+    cat README.md
+    exit
+fi
