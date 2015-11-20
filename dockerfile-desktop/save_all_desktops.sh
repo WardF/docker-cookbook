7b18a9a0fad4b635b6c007fad01721e1f6778f54
Ward Fisher <wfisher@ucar.edu>
2015-11-20 15:00:25 -0700

    Added an explicit tag for normal desktop.

--- dockerfile-desktop/save_all_desktops.sh
+++ dockerfile-desktop/save_all_desktops.sh
@@ -6,7 +6,7 @@
 set -e
 set -x
 
-docker save -o wardf_desktop.tar wardf/desktop
+docker save -o wardf_desktop.tar wardf/desktop:latest
 docker save -o wardf_desktop-32.tar wardf/desktop:32
 docker save -o wardf_desktop-openmpi.tar wardf/desktop:openmpi
 docker save -o wardf_desktop-openmpi32.tar wardf/desktop:openmpi32
